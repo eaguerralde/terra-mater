490956d13d9409300e7a07300bed8b5f71b5216c
Eduardo Aguerralde <eaguerralde@gmail.com>
2023-02-17 09:52:33 +0000

    Added typeORM repository setup for User entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,21 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn, BeforeUpdate } from 'typeorm';
+
+@Entity('user')
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    public name: string;
+
+    @Column({ type: 'timestamp', default: () => "CURRENT_TIMESTAMP"})
+    created: Date;
+
+    @Column({ type: 'timestamp', default: () => "CURRENT_TIMESTAMP"})
+    updated: Date;
+
+    @BeforeUpdate()
+    updateTimestamp() {
+        this.updated = new Date;
+    }
+}
